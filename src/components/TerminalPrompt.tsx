fe62ccbd96a64c7dd4077286c0c4d8e744c0954b
Jeff Mastry <jeff@mastry.net>
2023-11-06 07:05:21 -0500

    Use consistent hook file names

--- src/components/TerminalPrompt.tsx
+++ src/components/TerminalPrompt.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from "react";
-import useCommandBuffer from "../hooks/CommandBuffer";
+import useCommandBuffer from "../hooks/commandBuffer";
 
 export interface TerminalPromptProps {
     prompt: string,
